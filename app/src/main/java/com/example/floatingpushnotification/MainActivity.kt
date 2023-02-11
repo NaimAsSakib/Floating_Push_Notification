9d11cb3610b6538d74d72dc8bc3303968ee59652
sakib <rifat.sakib90@gmail.com>
2023-02-11 13:35:04 +0600

    Sent push notification from postman with firebase server key as Authorization, and took device specific access token from device and sent from postman

--- app/src/main/java/com/example/floatingpushnotification/MainActivity.kt
+++ app/src/main/java/com/example/floatingpushnotification/MainActivity.kt
@@ -21,41 +21,13 @@ import androidx.core.content.ContextCompat
 import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
-    //private lateinit var txt:TextView
 
     @SuppressLint("MissingInflatedId", "MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /*tvPlay.setOnClickListener {
-           var mediaPlayer= MediaPlayer.create(this,R.raw.short_sms_tone)
-           mediaPlayer.start()
-       }*/
 
-        /* txt.setOnClickListener {
-            //sendInstantPushNotification()
-
-        }*/
-
-
-        /*@SuppressLint("MissingPermission")
-    private fun sendInstantPushNotification(){
-        val builder = NotificationCompat.Builder(this, "channel_id")
-            .setSmallIcon(R.drawable.ic_tiger)
-            .setContentTitle("Notification Title")
-            .setContentText("Notification text")
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-
-        val notificationManager = NotificationManagerCompat.from(this)
-        notificationManager.notify(1,builder.build())
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel("channel_id", "channel_name", NotificationManager.IMPORTANCE_DEFAULT)
-            val notificationManager = getSystemService(NotificationManager::class.java)
-            notificationManager?.createNotificationChannel(channel)
-        }
-    }*/
 
 
         val txt =findViewById<TextView>(R.id.qwe)
--- app/src/main/java/com/example/floatingpushnotification/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/floatingpushnotification/MyFirebaseMessagingService.kt
@@ -24,56 +24,6 @@ import kotlin.random.Random
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
-   /* override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        if(remoteMessage.getNotification() != null){
-            generateNotification(remoteMessage.notification!!.body!!)
-        }
-    }*//*
-
-    @SuppressLint("RemoteViewLayout")
-    fun getRemoteView(message: String): RemoteViews {
-
-        val remoteView = RemoteViews("com.example.pushnotification",R.layout.notification)
-
-        //remoteView.setTextViewText(R.id.title,title)
-        remoteView.setTextViewText(R.id.description,message)
-        remoteView.setImageViewResource(R.id.app_logo,R.drawable.ic_tiger)
-
-        return remoteView
-    }
-
-    @SuppressLint("UnspecifiedImmutableFlag")
-    fun generateNotification(message:String){
-        val intent = Intent(this,MainActivity :: class.java)
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-
-        val pendingIntent = PendingIntent.getActivity(this,0,intent, PendingIntent.FLAG_ONE_SHOT)
-
-        //CHANNEL ID,NAME
-
-        var builder: NotificationCompat.Builder = NotificationCompat.Builder(applicationContext, channelId)
-            .setSmallIcon(R.drawable.ic_tiger)
-            .setAutoCancel(true)
-            .setVibrate(longArrayOf(1000,1000,1000,1000,1000))
-            .setOnlyAlertOnce(true)
-            .setContentIntent(pendingIntent)
-            .setSound(Uri.parse("${ContentResolver.SCHEME_ANDROID_RESOURCE}://${this@MyFirebaseMessagingService.packageName}/${ R.raw.short_sms_tone}"))
-
-        builder = builder.setContent(getRemoteView(message))
-
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            val notificationChannel = NotificationChannel(channelId, channelName,
-                NotificationManager.IMPORTANCE_HIGH)
-            notificationManager.createNotificationChannel(notificationChannel)
-        }
-
-        notificationManager.notify(0,builder.build())
-
-        MediaPlayer.create(applicationContext, R.raw.short_sms_tone).start()
-    }*/
-
     override fun onNewToken(token: String) {
 
     }
